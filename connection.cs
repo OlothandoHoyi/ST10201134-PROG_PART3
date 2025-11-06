d8b9660deb1338b1d525f527b6cc042fff2f19e7
OlothandoHoyi <163025214+OlothandoHoyi@users.noreply.github.com>
2025-11-06 14:43:30 +0200

    Add files via upload

---
+++ connection.cs
@@ -0,0 +1,13 @@
+﻿namespace POE_part2
+{
+    public class connection
+    {
+        //return connection string
+        public string Connecting()
+        { 
+            //then return the connection
+            return "Data Source=(localdb)\\demo_p2;Initial Catalog=demo_db;";
+        }
+
+    }
+}
